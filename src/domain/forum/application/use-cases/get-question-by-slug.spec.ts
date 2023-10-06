0d652ef9150a07786a46556f33a63a1a3845d125
brunocotto <bruno.ottoo@gmail.com>
2023-10-05 22:03:39 -0300

    Fórum com DDD & Clean Architecture: Factories de testes

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -1,8 +1,7 @@
-import { UniqueEntityId } from '@/core/entities/unique-entity-id';
-import { Question } from '../../enterprise/entities/question';
-import { Slug } from '../../enterprise/entities/value-objects/slug';
 import { GetQuestionBySlugUseCase } from './get-question-by-slug';
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'test/factories/make-question';
+import { Slug } from '../../enterprise/entities/value-objects/slug';
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: GetQuestionBySlugUseCase
@@ -14,11 +13,8 @@ describe('Get Question By Slug', () => {
   })
 
   it('should be able to get a question by slug', async () => {  
-    const newQuestion = Question.create({
-      title: 'Example question',
+    const newQuestion = makeQuestion({
       slug: Slug.create('example-question'),
-      authorId: new UniqueEntityId(),
-      content: 'Example content'
     })
 
     await inMemoryQuestionsRepository.create(newQuestion)
---
+++ test/factories/make-question.ts
@@ -0,0 +1,13 @@
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { Question, QuestionProps } from "@/domain/forum/enterprise/entities/question";
+
+export function makeQuestion(override: Partial<QuestionProps> = {}) {
+  const question = Question.create({
+    title: 'Example question',
+    authorId: new UniqueEntityId(),
+    content: 'Example content',
+    ...override
+  })
+
+  return question
+}
