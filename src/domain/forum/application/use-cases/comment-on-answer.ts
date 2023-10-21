814ebd708e93f79d2f0a960a206d596e82c10623
brunocotto <bruno.ottoo@gmail.com>
2023-10-21 11:18:46 -0300

    Caso de uso: Comentar na resposta

---
+++ src/domain/forum/application/use-cases/comment-on-answer.ts
@@ -0,0 +1,46 @@
+import { AnswerCommentsRepository } from './../repositories/answer-comments-repository';
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { AnswersRepository } from "../repositories/answers-repository";
+import { AnswerComment } from "../../enterprise/entities/answer-comment";
+
+
+interface CommentOnAnswerUseCaseRequest {
+  authorId: string
+  answerId: string
+  content: string
+}
+
+interface CommentOnAnswerUseCaseResponse {
+  answerComment: AnswerComment
+}
+
+export class CommentOnAnswerUseCase {
+  constructor(
+    private answersRepository: AnswersRepository,
+    private answerCommentsRepository: AnswerCommentsRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    answerId,
+    content,
+  }: CommentOnAnswerUseCaseRequest): Promise<CommentOnAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not a found.')
+    }
+
+    const answerComment = AnswerComment.create({
+      authorId: new UniqueEntityId(authorId),
+      answerId: new UniqueEntityId(answerId),
+      content,
+    })
+
+    await this.answerCommentsRepository.create(answerComment)
+
+    return {
+      answerComment,
+    }
+  }
+}
