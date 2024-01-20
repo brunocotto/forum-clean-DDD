8dada074d40bd48b299b153c8e1825503377a930
brunocotto <bruno.ottoo@gmail.com>
2024-01-20 10:26:09 -0300

    aggreate-root e Pattern: WatchedList implementados

--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -2,12 +2,14 @@ import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { Question } from "../../enterprise/entities/question";
 import { QuestionsRepository } from "../repositories/questions-repository";
 import { Either, right } from "@/core/either";
+import { QuestionAttachment } from "../../enterprise/entities/question-attachment";
 
 
 interface CreateQuestionUseCaseRequest {
   authorId: string
   title: string
   content: string
+  attachmentIds: string[]
 }
 
 type CreateQuestionUseCaseResponse = Either<
@@ -24,12 +26,22 @@ export class CreateQuestionUseCase {
     authorId,
     title,
     content,
+    attachmentIds,
   }: CreateQuestionUseCaseRequest): Promise<CreateQuestionUseCaseResponse> {
     const question = Question.create({
       authorId: new UniqueEntityId(authorId),
       title,
       content,
     })
+
+    const questionAttachment = attachmentIds.map((attachmentId) => {
+      return QuestionAttachment.create({
+        attachmentId: new UniqueEntityId(attachmentId),
+        questionId: question.id
+      })
+    })
+
+    question.attachment = questionAttachment
 
     await this.questionsRepository.create(question)
 
